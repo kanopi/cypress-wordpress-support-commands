6cf13b809faedc56d81190c25c1c88f1be6917e8
Paul Sheldrake <paul.sheldrake@gmail.com>
2026-02-23 09:24:19 -0800

    tweak save commands

--- save.js
+++ save.js
@@ -10,9 +10,13 @@
 Cypress.Commands.add('save', (viewPost = true) => {
   cy.get('.edit-post-header button').contains('Save').click()
   if (viewPost) {
-    cy.window().then((win) => {
+    // Use .should() so Cypress retries until the permalink updates from /auto-draft/
+    cy.window().should((win) => {
       const permalink = win.wp.data.select('core/editor').getPermalink()
       expect(permalink).to.be.a('string')
+      expect(permalink).to.not.include('/auto-draft')
+    }).then((win) => {
+      const permalink = win.wp.data.select('core/editor').getPermalink()
       cy.visit(permalink)
     })
   }
